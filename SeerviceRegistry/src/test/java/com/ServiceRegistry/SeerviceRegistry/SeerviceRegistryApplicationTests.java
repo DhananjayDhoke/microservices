7db2a5a155b87470df9b20de5330f28265733100
Dhananjay Dhoke <95958394+DhananjayDhoke@users.noreply.github.com>
2023-02-03 12:04:00 +0530

    service registry

---
+++ SeerviceRegistry/src/test/java/com/ServiceRegistry/SeerviceRegistry/SeerviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceRegistry.SeerviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
